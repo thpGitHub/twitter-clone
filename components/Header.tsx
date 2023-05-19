d6208723a8d380fd3431acdc7ee5bcc75e588d2c
thierry <thierrypoupon@hotmail.com>
2023-05-19 10:48:50 +0200

    working in progress on layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -18,8 +18,14 @@ const Header = ({label, showBackArrow}: HeaderProps) => {
     <div className="border-b-[1px] border-neutral-800 p-5">
         <div className="flex flex-row items-center gap-2">
             {showBackArrow && (
-                <BiArrowBack />
+                <BiArrowBack 
+                onClick={handleBack}
+                color="white"
+                size={20}
+                className="cursor-pointer hover:opacity-80 transition"
+                />
             )}
+            <h1 className="text-white text-xl font-semibold">{label}</h1>
         </div>
     </div>
   )
