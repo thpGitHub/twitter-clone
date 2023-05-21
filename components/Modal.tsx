3b14ae0fdfa3b5c8a40ae88f415a66ef3aeb66f7
thierry <thierrypoupon@hotmail.com>
2023-05-21 18:32:36 +0200

    working on Modal component

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,4 +1,6 @@
 import { useCallback } from "react"
+import {AiOutlineClose} from "react-icons/ai"
+import Button from "./Button"
 
 type ModalProps = {
     isOpen?: boolean
@@ -7,7 +9,7 @@ type ModalProps = {
     title?: string
     body?: React.ReactElement
     footer?: React.ReactElement
-    actionLabel?: string
+    actionLabel: string
     disable?: boolean
 
 }
@@ -16,20 +18,104 @@ const Modal = ({isOpen, onClose, onSubmit, title, body, footer, actionLabel, dis
 
   const handleClose = useCallback(() => {       
     if (disable) return
-
     onClose()
   }, [disable, onClose])
 
   const handleSubmit = useCallback(() => {
     if (disable) return
-
     onSubmit()
   }, [disable, onSubmit])
 
   if (!isOpen) return null  
 
   return (
-    <div>Modal</div>
+    <>
+      <div
+        className="
+          justify-center
+          items-center
+          flex
+          overflow-x-hidden
+          overflow-y-auto
+          inset-0
+          z-50 
+          outline-none
+          focus:outline-none
+          bg-neutral-800
+          bg-opacity-70
+          fixed
+        "
+      >
+        <div
+          className="
+          relative
+          w-full
+          h-full
+          my-6
+          mx-auto
+          lg:w-3/6
+          lg:max-w-3xl
+          lg:h-auto
+          "
+        >
+          {/*content*/}
+          <div 
+            className="
+              h-full
+              lg:h-auto
+              border-0
+              shadow-lg
+              rounded-lg
+              relative
+              flex
+              flex-col
+              w-full
+              bg-black
+              outline-none
+              focus:outline-none"
+          >
+            {/*header*/}
+            <div
+              className="
+                flex
+                ittems-center
+                justify-between
+                p-10
+                rounded-t"
+            >
+              <h3 className="text-3xl text-white font-semibold">{title}</h3>
+              <button
+                onClick={handleClose}
+                className="
+                  p-1
+                  ml-auto
+                  border-0
+                  text-white
+                  hover:opacity-70
+                  transition
+                ">
+                <AiOutlineClose size={20} />
+              </button>
+            </div>
+            {/*body*/}
+            <div className="relative p-10 flex-auto">
+              {body}
+            </div>
+            {/*footer*/}
+            <div className="flex flex-col gap-2 p-10">
+              <Button 
+                disabled
+                label={actionLabel}
+                secondary 
+                fullWidth 
+                large 
+                onClick={handleSubmit} />
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </>
   )
 }
 
